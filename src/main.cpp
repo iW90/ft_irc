3370806bdfc136a9e0fa91399b14eee21ee5c4ff
Marcos <marcos.chiu@gmail.com>
2024-11-02 11:56:21 -0300

    b

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: maalexan <maalexan@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/02 10:52:25 by maalexan          #+#    #+#             */
-/*   Updated: 2024/11/02 10:52:27 by maalexan         ###   ########.fr       */
+/*   Updated: 2024/11/02 11:52:22 by maalexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,27 +18,28 @@
 #include <netinet/in.h>
 
 int main() {
-    // Create a socket
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+  // Create a socket
+  int server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Bind the socket to an address and port
-    sockaddr_in address;
-    std::memset(&address, 0, sizeof(address));
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
+  // Bind the socket to an address and port
+  sockaddr_in address;
+  std::memset(&address, 0, sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(8080);
 
-    // Listen for connections
-    listen(server_fd, 1);
+  bind(server_fd, (struct sockaddr *)&address, sizeof(address));
 
-    // Accept a connection and send a response
-    int client_fd = accept(server_fd, NULL, NULL);
-    const char *response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
-    send(client_fd, response, std::strlen(response), 0);
+  // Listen for connections
+  listen(server_fd, 1);
 
-    // Close the connection
-    close(client_fd);
-    close(server_fd);
-    return 0;
+  // Accept a connection and send a response
+  int client_fd = accept(server_fd, NULL, NULL);
+  const char *response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\n\r\nHello, World!";
+  send(client_fd, response, std::strlen(response), 0);
+
+  // Close the connection
+  close(client_fd);
+  close(server_fd);
+  return 0;
 }
