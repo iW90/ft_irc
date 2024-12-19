981578ca909d6c0338f604acd16086b14716e498
Marcos <marcos.chiu@gmail.com>
2024-12-19 19:02:49 -0300

    httpserver enh

--- src/HttpServer.cpp
+++ src/HttpServer.cpp
@@ -20,8 +20,39 @@ HttpServer::HttpServer() : server_fd(-1) {
   memset(&address, 0, sizeof(address));
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
-  address.sin_port = htons(DEFAULT_PORT);
+  setPort(DEFAULT_PORT);
 }
+
+HttpServer::HttpServer(uint16_t port) : server_fd(-1) {
+  memset(&address, 0, sizeof(address));
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  setPort(port);
+}
+
+HttpServer::HttpServer(const HttpServer& other) : server_fd(-1) {
+  std::memcpy(&address, &other.address, sizeof(address));
+  address.sin_port = 0;
+
+  uint16_t new_port = other.address.sin_port == 0 ? DEFAULT_PORT : ntohs(other.address.sin_port) + 1;
+  setPort(new_port);
+}
+
+HttpServer& HttpServer::operator=(const HttpServer& other) {
+  if (this != &other) {
+    if (server_fd != -1) {
+      close(server_fd);
+    }
+    std::memcpy(&address, &other.address, sizeof(address));
+    address.sin_port = 0;
+
+    uint16_t new_port = other.address.sin_port == 0 ? DEFAULT_PORT : ntohs(other.address.sin_port) + 1;
+    setPort(new_port);
+    server_fd = -1;
+    }
+  return *this;
+}
+
 
 HttpServer::~HttpServer() {
   if (server_fd != -1) {
@@ -29,28 +60,60 @@ HttpServer::~HttpServer() {
   }
 }
 
+HttpServer& HttpServer::setPort(uint16_t port) {
+  address.sin_port = htons(port);
+  return *this;
+}
+
+
 HttpServer& HttpServer::createServerSocket() {
   server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd == -1) {
-    throw std::runtime_error("Failed to create socket");
+    throw std::runtime_error("Http server failed to create socket");
   }
   return *this;
 }
 
 HttpServer& HttpServer::bindSocket() {
   if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
-    throw std::runtime_error("Failed to bind socket");
+    throw std::runtime_error("Http server failed to bind socket");
+  }
+  return *this;
+}
+
+HttpServer& HttpServer::bindSocket() {
+  if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) == -1) {
+    throw std::runtime_error("Http server failed to bind socket");
+  }
+
+  socklen_t len = sizeof(address);
+  if (getsockname(server_fd, (struct sockaddr*)&address, &len) == -1) {
+    throw std::runtime_error("Http server failed to retrieve assigned port");
+  }
+  return *this;
+}
+
+
+HttpServer& HttpServer::setSocketOptions() {
+  int opt = 1;
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+    throw std::runtime_error("Http server failed to set socket options");
   }
   return *this;
 }
 
 HttpServer& HttpServer::listenForConnections() {
   if (listen(server_fd, MAX_BACKLOG) == -1) {
-    throw std::runtime_error("Failed to listen on socket");
+    throw std::runtime_error("Http server failed to listen on socket");
   }
   return *this;
 }
 
 void HttpServer::start() {
-  createServerSocket().bindSocket().listenForConnections();
+  createServerSocket().setSocketOptions().bindSocket().listenForConnections();
+}
+
+HttpServer& HttpServer::useIPv6() {
+  address.sin_family = AF_INET6;
+  return *this;
 }
